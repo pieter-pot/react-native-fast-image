aa827ffc77afecc3e484ca84827c43c6de44eb73
Herman <herman@q42.nl>
2022-07-18 16:24:26 +0200

    wip update

--- ReactNativeFastImageExample/metro.config.js
+++ ReactNativeFastImageExample/metro.config.js
@@ -4,8 +4,23 @@
  *
  * @format
  */
+const path = require("path")
+const extraNodeModules = {
+    'react-native-fast-image': path.resolve(__dirname, '..'),
+}
+console.warn({extraNodeModules})
 
 module.exports = {
+    resolver: {
+        // https://dushyant37.medium.com/how-to-import-files-from-outside-of-root-directory-with-react-native-metro-bundler-18207a348427
+        extraNodeModules: new Proxy(extraNodeModules, {
+            get: (target, name) =>
+            //redirects dependencies referenced from extraNodeModules/ to local node_modules
+            name in target
+                ? target[name]
+                : path.join(process.cwd(), `node_modules/${name}`),
+        }),
+    },
     transformer: {
         getTransformOptions: async () => ({
             transform: {
